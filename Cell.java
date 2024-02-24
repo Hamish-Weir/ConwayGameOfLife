4c0d6cee7724075cbd96488b456a9d8413781a01
c93350 <hamish.weir@student.manchester.ac.uk>
2024-02-24 12:54:51 +0000

    initial commit: Cell.java, Board.java

---
+++ Cell.java
@@ -0,0 +1,21 @@
+package ConwayGameOfLife;
+
+public class Cell {
+    private int state = 0;
+
+
+    Cell()
+    {
+
+    }
+
+    public int getState()
+    {
+        return state;
+    }
+
+    public void setState(int State)
+    {
+        state = State;
+    }
+}
