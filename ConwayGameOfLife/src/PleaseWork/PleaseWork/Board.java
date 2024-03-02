440d68121a713c8998dddc58e23edf38da1f96c1
c93350 <hamish.weir@student.manchester.ac.uk>
2024-03-02 12:04:02 +0000

    works

--- ConwayGameOfLife/src/PleaseWork/PleaseWork/Board.java
+++ ConwayGameOfLife/src/PleaseWork/PleaseWork/Board.java
@@ -33,30 +33,39 @@ public class Board {
 
     public void updateBoard()
     {
+        int[][] nextState = new int[boardWidth][boardHeight];
         for(int i= 0 ; i < boardWidth;i++)
         {
-            for(int j= 0 ; j < boardWidth;j++)
+            for(int j= 0 ; j < boardHeight;j++)
             {
-
+                nextState[i][j] = getNextState(i, j);
+            }
+        }
+        for(int i= 0 ; i < boardWidth;i++)
+        {
+            for(int j= 0 ; j < boardHeight;j++)
+            {
+                setCell(i,j,nextState[i][j]);
             }
         }
     }
 
-    public boolean getNextState(int x, int y)
+    public int getNextState(int x, int y)
     {
+        //I Could use Bool but this makes it more expandable
         int neighbours = getNeighbours(x, y);
         int state = getCell(x, y).getState();
 
         if(state == 1)
         {
-            if (neighbours < 2){return false;}
-            if (neighbours >= 2 && neighbours <=3){return true;}
-            else {return false;}
+            if (neighbours < 2){return 0;}
+            if (neighbours >= 2 && neighbours <=3){return 1;}
+            else {return 0;}
         }
         else
         {
-            if (neighbours == 3){return true;}
-            else {return false;}
+            if (neighbours == 3){return 1;}
+            else {return 0;}
         }
     } 
 
@@ -82,24 +91,48 @@ public class Board {
         return NoNeighbours;
     }
 
+    public void printBoard() 
+    {
+        for(int j = 0; j < boardHeight ;j++)
+        {
+            System.out.print("|");
+            for(int i = 0; i < boardWidth ;i++)
+            {
+                if(getCell(i, j).getState()== 1){System.out.print("■");}
+                if(getCell(i, j).getState()== 0){System.out.print(".");}
+            }
+            System.out.println("|");
+        }
+        System.out.println("");
+    }
 
     public static void main(String[] args) {
         
-        Board B = new Board(3, 3);
+        Board B = new Board(100, 10);
 
-        B.setCell(0, 0, 1);
-        B.setCell(1, 0, 1);
-        B.setCell(2, 0, 1);
+        B.setCell(0, 0, 0);
+        B.setCell(1, 0, 0);
+        B.setCell(2, 0, 0);
+
         B.setCell(0, 1, 1);
-        B.setCell(1, 1, 1);
+        B.setCell(1, 1, 0);
         B.setCell(2, 1, 1);
-        B.setCell(0, 2, 1);
+
+        B.setCell(0, 2, 0);
         B.setCell(1, 2, 1);
-        B.setCell(2,2, 1);
+        B.setCell(2, 2, 1);
 
-        System.out.println(B.getNeighbours(1,1));
+        B.setCell(0, 3, 0);
+        B.setCell(1, 3, 1);
+        B.setCell(2, 3, 0);
 
-        System.out.print(B.getNextState(0,0));
+        do{
+        B.printBoard();
+        B.updateBoard();
+        System.console().readLine();
+        }
+        while(true);
+
 
     }
 }
